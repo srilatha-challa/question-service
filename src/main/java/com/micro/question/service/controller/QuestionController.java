441e5817dc44ee2705438c295dfc05d452c3bb25
srilatha-challa <srilathach03701@gmail.com>
2025-04-14 15:22:09 +0530

    refactoring description

--- src/main/java/com/micro/question/service/controller/QuestionController.java
+++ src/main/java/com/micro/question/service/controller/QuestionController.java
@@ -37,46 +37,45 @@ public class QuestionController {
         this.questionService = questionService;
     }
     
-    @GetMapping("all")
     @Operation(summary = "Get All Questions")
+    @GetMapping("all")
     public ResponseEntity<List<QuestionResponse>> findAllQuestions(){
         return new ResponseEntity<>(questionService.findAllQuestions(), HttpStatus.OK);   
     }
     
-    @GetMapping("category/{category}")
     @Operation(summary = "Get Questions for given Category")
+    @GetMapping("category/{category}")
     public ResponseEntity<List<QuestionResponse>> findQuestionsByCategory(@PathVariable("category") String category){
         return new ResponseEntity<>(questionService.findQuestionsByCategory(category), HttpStatus.OK);
     }
     
-    @PostMapping("add")
     @Operation(summary = "Add Questions")
+    @PostMapping("add")
     public ResponseEntity<String> addQuestions(@Valid @RequestBody List<QuestionRequest> questionReqList){
     	return new ResponseEntity<>(questionService.addQuestions(questionReqList), HttpStatus.CREATED);
     }
     
-    @DeleteMapping("delete")
     @Operation(summary = "Delete Questions")
+    @DeleteMapping("delete")
     public ResponseEntity<String> deleteQuestions(@NotEmpty(message = "Id list {should.not.empty}")@RequestBody List<Integer> idList) throws NotFoundException{
     	return new ResponseEntity<>(questionService.deleteQuestions(idList), HttpStatus.OK);   	
     }
     
-    @GetMapping("generate")
     @Operation(summary = "Get Questions For Quiz")
+    @GetMapping("generate")
     public ResponseEntity<List<Integer>> getQuestionForQuiz(@RequestParam("category") String category, 
     										@RequestParam("numOfQuestions") Integer numOfQuestions){
     	return new ResponseEntity<>(questionService.getQuestionForQuiz(category, numOfQuestions), HttpStatus.OK);   	    	
     }
     
-    @PostMapping("getQuestions")
     @Operation(summary = "Get Questions For Id's")
+    @PostMapping("getQuestions")
     public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@NotEmpty(message = "Id list {should.not.empty}") @RequestBody List<Integer> idList) throws NotFoundException{
     	return new ResponseEntity<>(questionService.getQuestionsFromId(idList), HttpStatus.OK);   	    	   	
     }
     
-    
-    @PostMapping("getScore")
     @Operation(summary = "Get Score For Quiz")
+    @PostMapping("getScore")
     public ResponseEntity<Integer> getScore(@Valid @RequestBody List<AnswersRequest> answersRequests){
     	return new ResponseEntity<>(questionService.getScore(answersRequests), HttpStatus.OK);
     }
